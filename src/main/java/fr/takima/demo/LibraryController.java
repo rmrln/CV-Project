506e0919fce61f849e9a6dd2790417aba2c0b0e9
Antoine QUETIN <antoine.quetin@hotmail.com>
2019-09-15 14:57:39 +0200

    v2 - Insertion d'un header

--- src/main/java/fr/takima/demo/LibraryController.java
+++ src/main/java/fr/takima/demo/LibraryController.java
@@ -2,12 +2,11 @@ package fr.takima.demo;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.springframework.web.servlet.view.RedirectView;
+
+import java.util.List;
 
 /**
  *
@@ -24,7 +23,7 @@ public class LibraryController {
 
     @GetMapping
     public String homePage(Model m) {
-        m.addAttribute("users", userDAO.findAll());
+        //m.addAttribute("users", userDAO.findAll());
         return "index";
     }
 
@@ -32,6 +31,18 @@ public class LibraryController {
     public String addUserPage(Model m) {
         m.addAttribute("user", new User());
         return "new";
+    }
+
+    @GetMapping("/index")
+    public String indexPage(Model m) {
+
+        return "index";
+    }
+
+    @GetMapping("/list")
+    public String listCVs(Model m) {
+        m.addAttribute("users", userDAO.findAll());
+        return "list";
     }
 
     @PostMapping("/new")
@@ -43,9 +54,26 @@ public class LibraryController {
 
     @GetMapping("/delete/{id}")
     public RedirectView deleteUser(@ModelAttribute User user, RedirectAttributes attrs) {
-        attrs.addFlashAttribute("message", "Utilisateur supprimé avec succès");
+        attrs.addFlashAttribute("message", "CV supprimé avec succès");
         userDAO.deleteById(user.getId());
         return new RedirectView("/");
     }
+
+    @GetMapping("/consult/{id}")
+    public RedirectView consultCV(@ModelAttribute User user, RedirectAttributes attrs) {
+        userDAO.findById(user.getId());
+        return new RedirectView("consult");
+    }
+
+    @GetMapping("/modif/{id}")
+    public String viewCV(Model m,@PathVariable Long id, User user){
+        //List<User> lListeUser = userDAO.findById(id);
+        if(id.equals(user.getId())){
+            m.addAttribute("user", user.getId());
+        }
+        return "modif";
+    }
+
+
 
 }
