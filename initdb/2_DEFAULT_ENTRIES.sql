4c175ec76cee0f62d1226c6308d43d9816929c01
manitout <mikael.archas@wanadoo.fr>
2019-09-17 10:39:10 +0200

    commit bdd missing field

--- initdb/2_DEFAULT_ENTRIES.sql
+++ initdb/2_DEFAULT_ENTRIES.sql
@@ -1,3 +1,3 @@
-INSERT INTO defaultdb.users (id,cv_title, first_name, last_name, age,linkedin,mail,presentation,skill,language) VALUES (1,'CV de BG', 'Paul', 'Harrohide', 20, 'ilikfjdsh','mail','presentation','skill','language');
+INSERT INTO defaultdb.users (id,picture,cv_title, first_name, last_name, age,phone_number,adress,linkedin,mail,presentation,skill,hobby,language) VALUES (1,'Photo juste la','CV de BG', 'Paul', 'Harrohide', 20, 06313475,'9 villa Carmen' 'ilikfjdsh','mail','presentation','skill','language');
 INSERT INTO defaultdb.formations (id,FormationID, formation_title, formation_description,formation_name, formation_date) VALUES (1,1, 'Harry', 'Covert','formations', 25);
 INSERT INTO defaultdb.experiences (id,ExperienceID, experience_title, experience_description,experience_position,experience_name, experience_date) VALUES (1,1, 'Harry', 'Covert','experience','eefbgff', 25);
