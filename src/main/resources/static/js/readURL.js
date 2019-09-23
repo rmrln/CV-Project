34ebb48de8d4a86bc8b620e4efbea855194b4a74
Roxane Merlen <roxane.merlen@epfedu.fr>
2019-09-23 11:44:54 +0200

    new html v7.1

--- src/main/resources/static/js/readURL.js
+++ src/main/resources/static/js/readURL.js
@@ -38,3 +38,31 @@ $(document).ready(function () {
 
 });
 
+$(document).ready(function () {
+    var counter = 0;
+
+    $("#addrow2").on("click", function () {
+        var newRow = $("<tr>");
+        var cols = "";
+
+        cols += '<td><input type="text" class="form-control" name="nom' + counter + '"/></td>';
+        cols += '<td><input type="text" class="form-control" name="poste' + counter + '"/></td>';
+        cols += '<td><input type="text" class="form-control" name="description' + counter + '"/></td>';
+        cols += '<td><input type="text" class="form-control" name="date' + counter + '"/></td>';
+
+        cols += '<td><button class="ibtnDel2 btn btn-md btn-danger "><i class="fa fa-trash-o" aria-hidden="true"></i></button></td>';
+        newRow.append(cols);
+        $("table.order-list2").append(newRow);
+        counter++;
+    });
+
+
+
+    $("table.order-list2").on("click", ".ibtnDel2", function (event) {
+        $(this).closest("tr").remove();
+        counter -= 1
+    });
+
+
+});
+
