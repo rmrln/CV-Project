17ec903b71d73ed63ff374ec73bd9768ded65ddd
Roxane Merlen <roxane.merlen@epfedu.fr>
2019-09-25 00:00:43 +0200

    correction fichier js - suppression méthodes inutiles

--- src/main/resources/static/js/readURL.js
+++ src/main/resources/static/js/readURL.js
@@ -11,58 +11,5 @@ function readURL(input) {
     }
 }
 
-$(document).ready(function () {
-    var counter = 0;
 
-    $("#addrow").on("click", function () {
-        var newRow = $("<tr>");
-        var cols = "";
-
-        cols += '<td><input type="text" class="form-control" name="nom' + counter + '"/></td>';
-        cols += '<td><input type="text" class="form-control" name="description' + counter + '"/></td>';
-        cols += '<td><input type="text" class="form-control" name="date' + counter + '"/></td>';
-
-        cols += '<td><button class="ibtnDel btn btn-md btn-danger "><i class="fa fa-trash-o" aria-hidden="true"></i></button></td>';
-        newRow.append(cols);
-        $("table.order-list").append(newRow);
-        counter++;
-    });
-
-
-
-    $("table.order-list").on("click", ".ibtnDel", function (event) {
-        $(this).closest("tr").remove();
-        counter -= 1
-    });
-
-
-});
-
-$(document).ready(function () {
-    var counter = 0;
-
-    $("#addrow2").on("click", function () {
-        var newRow = $("<tr>");
-        var cols = "";
-
-        cols += '<td><input type="text" class="form-control" name="nom' + counter + '"/></td>';
-        cols += '<td><input type="text" class="form-control" name="poste' + counter + '"/></td>';
-        cols += '<td><input type="text" class="form-control" name="description' + counter + '"/></td>';
-        cols += '<td><input type="text" class="form-control" name="date' + counter + '"/></td>';
-
-        cols += '<td><button class="ibtnDel2 btn btn-md btn-danger "><i class="fa fa-trash-o" aria-hidden="true"></i></button></td>';
-        newRow.append(cols);
-        $("table.order-list2").append(newRow);
-        counter++;
-    });
-
-
-
-    $("table.order-list2").on("click", ".ibtnDel2", function (event) {
-        $(this).closest("tr").remove();
-        counter -= 1
-    });
-
-
-});
 
